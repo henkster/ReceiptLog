42f7d5ca4a0cb66e82beb1698fd05a5dad64b7db
Steve Henke <shenke@strategichealthservices.com>
2012-02-25 22:01:04 -0600

    Original commit.

---
+++ ReceiptLog/Areas/Mobile/Controllers/LogController.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+using ReceiptLog.Areas.Mobile.Models;
+
+namespace ReceiptLog.Areas.Mobile.Controllers
+{
+    public class LogController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new LogCreateViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(LogCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var rle = new ReceiptLogEntities();
+
+            rle.AddToLogs(new Log
+                                                   {
+                                                       AccountName = model.AccountName,
+                                                       Amount = model.Amount.Value,
+                                                       When = model.When,
+                                                       Where = model.Where,
+                                                       Why = model.Why
+                                                   });
+
+            rle.SaveChanges();
+
+            TempData["flash"] = "Logged";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ReceiptLog/Areas/Mobile/Models/LogCreateViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReceiptLog.Areas.Mobile.Models
+{
+    public class LogCreateViewModel
+    {
+        public LogCreateViewModel()
+        {
+            When = DateTime.Now;
+        }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Account Name")]
+        public string AccountName { get; set; }
+
+        [Required]
+        public decimal? Amount { get; set; }
+
+        private DateTime _when = DateTime.Now.Date;
+
+        public DateTime When
+        {
+            get { return _when.Date; }
+            set { _when = value.Date; }
+        }
+
+        [Required]
+        [StringLength(50)]
+        public string Where { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Why/Notes")]
+        public string Why { get; set; }
+    }
+}
